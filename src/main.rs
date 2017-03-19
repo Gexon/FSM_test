944650be80da59093b30d0598438bf5858697b62
Gexon <alimgan@gmail.com>
2017-03-20 02:57:31 +0500

    Опыты над конечными автоматами. Обертки и фабрика.

--- src/main.rs
+++ src/main.rs
@@ -49,10 +49,53 @@ impl From<BottleFillingMachine<Filling>> for BottleFillingMachine<Done> {
     }
 }
 
+impl From<BottleFillingMachine<Done>> for BottleFillingMachine<Waiting> {
+    fn from(val: BottleFillingMachine<Done>) -> BottleFillingMachine<Waiting> {
+        BottleFillingMachine {
+            shared_value: val.shared_value,
+            state: Waiting {
+                waiting_time: std::time::Duration::new(0, 0),
+            }
+        }
+    }
+}
+
 
 // В случае, если вы делаете это внутри функции, сигнатура которой ограничивает выходной тип:
 fn _transition_the_states(val: BottleFillingMachine<Waiting>) -> BottleFillingMachine<Filling> {
     val.into() // Мило, не правда ли?
+}
+
+
+// обертка для нашего конечного автомата
+enum BottleFillingMachineWrapper {
+    Waiting(BottleFillingMachine<Waiting>),
+    Filling(BottleFillingMachine<Filling>),
+    Done(BottleFillingMachine<Done>)
+}
+
+struct Factory {
+    bottle_filling_machine: BottleFillingMachineWrapper
+}
+
+impl Factory {
+    fn new() -> Self {
+        Factory {
+            bottle_filling_machine: BottleFillingMachineWrapper::Waiting(BottleFillingMachine::new(0))
+        }
+    }
+}
+
+
+impl BottleFillingMachineWrapper {
+    fn step(mut self) -> Self {
+        self = match self {
+            BottleFillingMachineWrapper::Waiting(val) => BottleFillingMachineWrapper::Filling(val.into()),
+            BottleFillingMachineWrapper::Filling(val) => BottleFillingMachineWrapper::Done(val.into()),
+            BottleFillingMachineWrapper::Done(val) => BottleFillingMachineWrapper::Waiting(val.into()),
+        };
+        self
+    }
 }
 
 
@@ -73,6 +116,10 @@ fn main() {
     // assert_eq!(bottle_filler.state.waiting_time, std::time::Duration::new(0, 0));
 
     let _bottle_filler = BottleFillingMachine::<Done>::from(bottle_filler);
+
+    // через обертки и фабрики
+    let mut the_factory = Factory::new();
+    the_factory.bottle_filling_machine = the_factory.bottle_filling_machine.step();
 }
 
 // тесты:
