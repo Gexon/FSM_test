5a340ae4894852cf743cee02284c94a4d67c5094
Gexon <alimgan@gmail.com>
2017-03-19 20:48:21 +0500

    опыты над конечными автоматами

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 struct Machine;
 
+enum BottleFillerState {
+    Waiting { waiting_time: std::time::Duration },
+    Filling { rate: usize },
+    Done
+}
+
+struct BottleFiller {
+    state: BottleFillerState
+}
+
+
 fn main() {
+    // наш базовый автомат как обыкновенная структура, которую можно создать и уничтожить.
     let machine = Machine;
     // `machine` будет уничтожена, когда выйдет за пределы области видимости.
 }
