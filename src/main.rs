081ae439adf04ed38280484e08464f49180055ef
Gexon <alimgan@gmail.com>
2017-03-20 01:54:27 +0500

    Опыты над конечными автоматами. Почти идеально.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 // Конечный автомат в состоянии «Заполнение» будет BottleStateMachine<Filling>
 struct BottleFillingMachine<S> {
     shared_value: usize,
-    state: S
+    _state: S
 }
 
 // Следующие состояния могут быть `S` в StateMachine<S>
 struct Waiting {
-    waiting_time: std::time::Duration
+    _waiting_time: std::time::Duration
 }
 
 struct Filling {
-    rate: usize
+    _rate: usize
 }
 
 struct Done;
@@ -20,8 +20,8 @@ impl BottleFillingMachine<Waiting> {
     fn new(shared_value: usize) -> Self {
         BottleFillingMachine {
             shared_value: shared_value,
-            state: Waiting {
-                waiting_time: std::time::Duration::new(0, 0)
+            _state: Waiting {
+                _waiting_time: std::time::Duration::new(0, 0)
             }
         }
     }
@@ -32,8 +32,8 @@ impl From<BottleFillingMachine<Waiting>> for BottleFillingMachine<Filling> {
     fn from(val: BottleFillingMachine<Waiting>) -> BottleFillingMachine<Filling> {
         BottleFillingMachine {
             shared_value: val.shared_value,
-            state: Filling {
-                rate: 1
+            _state: Filling {
+                _rate: 1
             }
         }
     }
@@ -44,13 +44,19 @@ impl From<BottleFillingMachine<Filling>> for BottleFillingMachine<Done> {
     fn from(val: BottleFillingMachine<Filling>) -> BottleFillingMachine<Done> {
         BottleFillingMachine {
             shared_value: val.shared_value,
-            state: Done
+            _state: Done
         }
     }
 }
 
 
+// В случае, если вы делаете это внутри функции, сигнатура которой ограничивает выходной тип:
+fn _transition_the_states(val: BottleFillingMachine<Waiting>) -> BottleFillingMachine<Filling> {
+    val.into() // Мило, не правда ли?
+}
+
+
 fn main() {
     let in_waiting = BottleFillingMachine::<Waiting>::new(0);
-    let in_filling = BottleFillingMachine::<Filling>::from(in_waiting);
+    let _in_filling = BottleFillingMachine::<Filling>::from(in_waiting);
 }
